a0e7f6be2124b9e4437ca79c3983fc041e525366
Teomayo <tode.crnobrnja@gmail.com>
2018-12-28 17:32:30 -0500

    slightly updated README

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # auto-garden
+
 Using Arduino to trigger automatic watering of plants
 
-## To Do
+## Reason
 
-- [ ] Modify chracter reading code to work for ints
-- [ ] clean up code into various functions for readabilty
-- [ ] implement calculations for creating viable range between dry / wet
-- [ ] change read times to be longer intervals
+Creating a calibratable device for constant feeback on state of garden / plants.
--- moisture_sensor.ino
+++ moisture_sensor.ino
@@ -1,13 +1,10 @@
+// intializing pins 
 const int analogInPin = A0; // main input 
 int sensorValue = 0;
 int outputValue = 0;
 const int redLED = 9;
 const int greenLED = 10;
 
-
-// self calibrating ints
-bool dryRead = 0;
-bool wetRead = 0;
 
 void setup() {
 	// initialize serial communication at 9600 bits per second:
@@ -16,27 +13,7 @@ void setup() {
 	pinMode(redLED, OUTPUT);
 	pinMode(greenLED, OUTPUT);
 	
-	// initialize sequence... need to add error function if intialize sequence doesnt complete
-	digitalWrite(greenLED, HIGH);
-	delay(150);
-	digitalWrite(greenLED, LOW);
-	digitalWrite(redLED, HIGH);
-	delay(150);
-	digitalWrite(greenLED, HIGH);
-
-	// self calibration sequence
-	Serial.print("Place sensor in dry soil and wait for reading");
-	// sensorValue = analogRead(analogInPin);
-	// Serial.print(sensorValue);
-	if (Serial.available() > 0) {
-        // read the incoming byte:
-        dryRead = Serial.read();
-        // say what you got:
-        Serial.print("Dry Reading: ");
-        Serial.println(dryRead);
-	}
-	
-
+	intitializeSequence();
 }
 
 // air: 255
@@ -49,14 +26,28 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-	// read the input on analog pin 0:
-	sensorValue = analogRead(analogInPin);
-	// print out the value you read:
-	outputValue = map(sensorValue, 0, 1023, 0, 255);
-	Serial.print("\n Sensor Value: ");
-	Serial.print(sensorValue);
-	Serial.print("\n Output Value: ");
-	Serial.print(outputValue);
+	sensorConversion();
+	moistureRange();
+	delay(150);        // delay in between reads for stability
+}
+
+
+// // ------------------------- Functions -------------------------------- //
+
+void intitializeSequence() {
+	// initialize sequence... need to add error function if intialize sequence doesnt complete
+	digitalWrite(greenLED, HIGH);
+	digitalWrite(redLED, LOW);
+	delay(1500);
+	digitalWrite(greenLED, LOW);
+	digitalWrite(redLED, HIGH);
+	delay(1500);
+	digitalWrite(greenLED, HIGH);
+	digitalWrite(redLED, LOW);
+}
+
+void moistureRange() {
+	// hardcoded values will be modified with self-calibrated values once number pad is integrated
 
 	if (outputValue < 80) {
 		// Switching LED's based off threshold
@@ -72,6 +63,11 @@ void loop() {
 		digitalWrite(redLED, HIGH);
 		Serial.println("\n NEEDS WATERING");
 	}
+}
 
-	delay(150);        // delay in between reads for stability
+void sensorConversion() {
+	// read the input on analog pin 0:
+	sensorValue = analogRead(analogInPin);
+	// convert to range of 0 - 255 
+	outputValue = map(sensorValue, 0, 1023, 0, 255);
 }

e795bd5314db84ef08de85f2ea3b262be14ed77a
Teomayo <tode.crnobrnja@gmail.com>
2019-05-24 22:50:46 -0400

    adding logic for MOSFET switch

--- moisture_sensor/moisture_sensor.ino
+++ moisture_sensor/moisture_sensor.ino
@@ -4,7 +4,7 @@ int sensorValue = 0;
 int outputValue = 0;
 const int redLED = 9;
 const int greenLED = 10;
-
+const int nMOS = 3; //variable for pin connected to MOSFET
 
 void setup() {
 	// initialize serial communication at 9600 bits per second:
@@ -12,14 +12,15 @@ void setup() {
 	// initializes pins as outputs
 	pinMode(redLED, OUTPUT);
 	pinMode(greenLED, OUTPUT);
+  pinMode(nMOS, OUTPUT);
 	
 	intitializeSequence();
 }
 
-// air: 255
-// dry soil: > 150
-// moist soil: 80 - 150
-// wet soil: < 80
+
+// dry soil: > 80
+// moist soil: 50 - 80
+// wet soil: < 50
 // in terms of outputValue
 
 
@@ -28,7 +29,7 @@ void setup() {
 void loop() {
 	sensorConversion();
 	moistureRange();
-	delay(150);        // delay in between reads for stability
+	delay(50);        // delay in between reads for stability
 }
 
 
@@ -38,30 +39,33 @@ void intitializeSequence() {
 	// initialize sequence... need to add error function if intialize sequence doesnt complete
 	digitalWrite(greenLED, HIGH);
 	digitalWrite(redLED, LOW);
-	delay(1500);
-	digitalWrite(greenLED, LOW);
-	digitalWrite(redLED, HIGH);
-	delay(1500);
-	digitalWrite(greenLED, HIGH);
-	digitalWrite(redLED, LOW);
+	delay(100);
+  digitalWrite(greenLED, LOW);
+  digitalWrite(redLED, LOW);
+  delay(100);
+  digitalWrite(greenLED, HIGH);
+  digitalWrite(redLED, LOW);
 }
 
 void moistureRange() {
 	// hardcoded values will be modified with self-calibrated values once number pad is integrated
 
-	if (outputValue < 80) {
+	if (outputValue <= 50) {
 		// Switching LED's based off threshold
 		digitalWrite(redLED, HIGH);
 		digitalWrite(greenLED, HIGH);
 		Serial.println("\n TOO WET");
-	} else if (outputValue > 80 & outputValue < 150) {
+    digitalWrite(nMOS, LOW);
+	} else if (outputValue > 50 & outputValue < 80) {
 		digitalWrite(greenLED, HIGH);
 		digitalWrite(redLED, LOW);
 		Serial.println("\n PERFECT");
+    digitalWrite(nMOS, LOW);
 	} else {
 		digitalWrite(greenLED, LOW);
 		digitalWrite(redLED, HIGH);
 		Serial.println("\n NEEDS WATERING");
+    digitalWrite(nMOS, HIGH);
 	}
 }
 
@@ -70,4 +74,5 @@ void sensorConversion() {
 	sensorValue = analogRead(analogInPin);
 	// convert to range of 0 - 255 
 	outputValue = map(sensorValue, 0, 1023, 0, 255);
+  Serial.println(outputValue);
 }
